a12c468c7cd8c743fffa70f1e8ba205b76ce8992
Annas-khan006 <khanannas006@gmail.com>
2024-09-22 21:30:34 +0500

    pushed

--- src/app.js
+++ src/app.js
@@ -10,12 +10,15 @@ app.use(cors({
 }))
 
 app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({
-    extended: true,
-    limit: "16kb"
-}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+import userRouter from './routes/users.routes.js'
+
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 
 export { app }
